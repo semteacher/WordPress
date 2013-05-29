96e3e942be6c9b5f98a9f9ddd66c999f769fc652
semteacher <semteacher@gmail.com>
2013-05-29 17:48:12 +0300

    neoyoo theme was added
    Signed-off-by: semteacher <semteacher@gmail.com>

---
+++ wp-content/themes/neoyootheme/functions.php
@@ -0,0 +1,13 @@
+<?php
+/**
+* @package   Neo
+* @author    YOOtheme http://www.yootheme.com
+* @copyright Copyright (C) YOOtheme GmbH
+* @license   YOOtheme Proprietary Use License (http://www.yootheme.com/license)
+*/
+
+// load config
+require_once(dirname(__FILE__).'/config.php');include'warp/menus/init.php';
+
+require_once(dirname(__FILE__).'/default-derwidget.php');
+
