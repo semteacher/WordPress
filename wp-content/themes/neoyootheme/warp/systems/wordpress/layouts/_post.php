ad9b11e725c43b5962c5a7133b33f955e4d254c9
semteacher <semteacher@gmail.com>
2013-06-10 13:01:54 +0300

    change post the create/update date displaying
    Signed-off-by: semteacher <semteacher@gmail.com>

--- wp-content/themes/neoyootheme/warp/systems/wordpress/layouts/_post.php
+++ wp-content/themes/neoyootheme/warp/systems/wordpress/layouts/_post.php
@@ -4,8 +4,9 @@
 	
 	<!--<p class="meta"><?php printf(__('Written by %s on %s. Posted in %s', 'warp'), '<a href="'.get_author_posts_url(get_the_author_meta('ID')).'" title="'.get_the_author().'">'.get_the_author().'</a>', get_the_date(), get_the_category_list(', ')); ?></p>
 	-->
-	<p class="meta"><?php printf(__('<font color=green> Дата публікації </font><font color=black><b> %s. </b></font><font color=green>Категорія %s </font>', 'warp'), get_the_date(), get_the_category_list(', ')); ?></p>
-	
+    <!--TDMU-->
+	<p class="meta"><?php printf(__('<font color=green> Опубліковано </font><font color=black><b> %s. </b></font><font color=green> Оновлено </font><font color=black><b> %s. </b></font><font color=green>Категорія %s </font>', 'warp'), get_the_date(), get_the_modified_date(), get_the_category_list(', ')); ?></p>
+	<!--TDMU-->
 	<div class="content"><?php the_content(''); ?></div>
 
 	<p class="links">
